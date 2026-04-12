49caa58114204fee0e2f22294b897bc5ea8bd101
krishna <venkatakrishnaredd450@gmail.com>
2026-04-12 14:24:43 +0530

    mongodb installing

--- mongodb.sh
+++ mongodb.sh
@@ -24,15 +24,15 @@ Validate(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 Validate $? "copying mongorepo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$log_file
 Validate $? "installing mongodb"
 
-systemctl enable mongod 
-systemctl start mongod 
+systemctl enable mongod &>>$log_file
+systemctl start mongod &>>$log_file
 Validate $? "enabling and starting"
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 Validate $? "connecting to all user"
 
-systemctl restart mongod
+systemctl restart mongod &>>$log_file
 Validate $? "restarting "
